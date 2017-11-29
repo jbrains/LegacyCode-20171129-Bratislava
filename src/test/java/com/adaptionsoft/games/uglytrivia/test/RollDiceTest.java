f54b8234522f96e1077c1a843b6eef5dbc2dda72
J. B. Rainsberger <me@jbrains.ca>
2017-11-29 14:48:28 +0100

    Documented another example of how the first player moves on the first turn.

--- src/test/java/com/adaptionsoft/games/uglytrivia/test/RollDiceTest.java
+++ src/test/java/com/adaptionsoft/games/uglytrivia/test/RollDiceTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class RollDiceTest {
     @Test
-    public void howTheFirstPlayerMovesOnTheFirstTurn() throws Exception {
+    public void howTheFirstPlayerMovesOnTheFirstTurnRolling1() throws Exception {
         class TestableGame extends Game {
             public TestableGame() {
                 super();
@@ -26,5 +26,28 @@ public class RollDiceTest {
         final TestableGame game = new TestableGame();
         game.roll(1);
         Assert.assertEquals(1, game.findPlaceForPlayer(0));
+    }
+
+    @Test
+    public void howTheFirstPlayerMovesOnTheFirstTurnRolling6() throws Exception {
+        class TestableGame extends Game {
+            public TestableGame() {
+                super();
+                add("::irrelevant player name::");
+            }
+
+            @Override
+            protected void reportMessage(final String message) {
+                // Shut up.
+            }
+
+            public int findPlaceForPlayer(int playerIndex) {
+                return places[playerIndex];
+            }
+        }
+
+        final TestableGame game = new TestableGame();
+        game.roll(6);
+        Assert.assertEquals(6, game.findPlaceForPlayer(0));
     }
 }
